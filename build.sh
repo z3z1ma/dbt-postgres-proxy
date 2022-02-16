10793af09ea4dac6a49f30873df8cf94ed2467f7
z3z1ma <butler.alex2010@gmail.com>
2022-02-16 11:29:58 -0700

    ship precompiled binaries for linux

--- build.sh
+++ build.sh
@@ -6,6 +6,6 @@ do
         CGO_ENABLED=0 \
         GOOS=${os} \
         GOARCH=${arch} \
-        go build -o dbt-pg-proxy-${os}-${arch} cmd/main.go
+        go build -o ./bin/dbt-pg-proxy-${os}-${arch} cmd/main.go
     done
 done
